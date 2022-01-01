c0b2c2cb30f88d303114575f73cdd0d841e3dd79
Josh Hendricks <jhendricks123@gmail.com>
2022-01-01 11:21:00 -0800

    Initial commit

---
+++ build.ps1
@@ -0,0 +1,34 @@
+[CmdletBinding()]
+param(
+    [Parameter()]
+    [ValidateSet('Serve', 'Build', 'Publish')]
+    [string]
+    $Task = 'Serve',
+
+    [Parameter()]
+    [switch]
+    $UseInsiders
+)
+
+docker pull ghcr.io/joshooaj/mkdocs-material-insiders:latest
+
+switch ($Task) {
+    'Serve' {
+        if ($UseInsiders) {
+            docker run --rm -it -p 8000:8000 -v $PWD`:/docs ghcr.io/joshooaj/mkdocs-material-insiders serve --config-file mkdocs.insiders.yml --dev-addr 0.0.0.0:8000
+        }
+        else {
+            docker run --rm -it -p 8000:8000 -v $PWD`:/docs ghcr.io/joshooaj/mkdocs-material-insiders serve --dev-addr 0.0.0.0:8000
+        }
+    }
+
+    'Build' {
+        if ($UseInsiders) {
+            docker run --rm -v $PWD`:/docs ghcr.io/joshooaj/mkdocs-material-insiders build --config-file mkdocs.insiders.yml
+        }
+        else {
+            docker run --rm -v $PWD`:/docs ghcr.io/joshooaj/mkdocs-material-insiders build
+        }
+    }
+    Default {}
+}
